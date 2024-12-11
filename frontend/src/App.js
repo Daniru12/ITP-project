7a024bd7c33ac57afaeb512b5298034f1f7cd6e1
Lasan Navodya <156741059+LaSan99@users.noreply.github.com>
2024-12-12 00:13:45 +0530

    Sample Web app

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import FeaturedProducts from './components/FeaturedProducts';
+import Newsletter from './components/Newsletter';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Header />
+      <Hero />
+      <FeaturedProducts />
+      <Newsletter />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
