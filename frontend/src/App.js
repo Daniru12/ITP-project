d8f4f350b824ce20fb27bf532609562ad638d9e3
Lasan Navodya <156741059+LaSan99@users.noreply.github.com>
2024-12-17 23:04:20 +0530

    added about and contact

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import FeaturedProducts from './components/FeaturedProducts';
 import Newsletter from './components/Newsletter';
 import Footer from './components/Footer';
+import About from './components/About';
+import Contact from './components/Contact';
 
-//test
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <Header />
-      <Hero />
-      <FeaturedProducts />
-      <Newsletter />
-      <Footer />
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Header />
+        <Routes>
+          <Route path="/" element={<><Hero /><FeaturedProducts /><Newsletter /></>} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
